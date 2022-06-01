45a7f7126bdf2dece9b9df67bfbe8781b0c25efa
vvt2001 <81843040+vvt2001@users.noreply.github.com>
2022-06-01 14:23:48 +0700

    Create video player view (#4)
    * tạo view video player
    
    view chạy video ngẫu nhiên từ thư viện trong trạng thái lặp lại
    
    * tách hàm ra thay vì dùng block
    
    tách hàm setupLoopVideoView từ block playLoopVideo (đồng thời đổi tên cho phù hợp)
    
    * private outlet
    
    * bỏ if else thừa

--- GIFCopy/ViewController.swift
+++ GIFCopy/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var videoView: UIView!
+    
+    private var videoPHAssets = [PHAsset]()
+    private var videoAVAsset: AVAsset?
+    private var playerLooper: AVPlayerLooper?
+    
+    func setupLoopVideoView(){
+        let randomVideoAssets = self.videoPHAssets.randomElement()
+        randomVideoAssets?.getAVAsset(completionHandler: { asset in
+            //setup player
+            let playerItem = AVPlayerItem(asset: asset!)
+            let randomVideoPlayer = AVQueuePlayer(playerItem: playerItem)
+            let playerLayer = AVPlayerLayer(player: randomVideoPlayer)
+            
+            //setup player layer layout
+            playerLayer.frame = self.videoView.bounds
+            playerLayer.videoGravity = .resize
+            self.videoView.layer.addSublayer(playerLayer)
+            
+            //loop with queue player
+            self.playerLooper = AVPlayerLooper(player: randomVideoPlayer, templateItem: playerItem)
+            
+            //play video
+            randomVideoPlayer.play()
+        })
+    }
+    
+    private func loadAssetFromPhotos(){
+        PHPhotoLibrary.requestAuthorization{ status in
+            if status == .authorized {
+                let videoAssets = PHAsset.fetchAssets(with: PHAssetMediaType.video, options: nil)
+                videoAssets.enumerateObjects{ (object, _, _) in
+                    self.videoPHAssets.append(object)
+                }
+                DispatchQueue.main.async {
+                    self.setupLoopVideoView()
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadAssetFromPhotos()
     }
-
-
 }
 
