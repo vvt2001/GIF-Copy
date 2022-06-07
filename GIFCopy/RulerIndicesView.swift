794518708aedfcbf0042c6738f42b0e5d63e2c30
Vu Viet Thang <vuvietthang1508@gmail.com>
2022-06-07 16:47:32 +0700

    chỉnh sửa code trong hàm draw cho gọn

--- GIFCopy/RulerIndicesView.swift
+++ GIFCopy/RulerIndicesView.swift
@@ -13,22 +13,18 @@ class RulerIndicesView: UIView{
     
     override func draw(_ rect: CGRect) {
         guard let context = UIGraphicsGetCurrentContext() else {return}
-        
+        var rectangle: CGRect
         for index in 0...240{
             if index % 10 == 0{
-                let rectangle = CGRect(x: 10+10*index, y: (superviewHeight - 20) / 2, width: 2, height: 20)
-                context.setFillColor(UIColor.white.cgColor)
-                context.addRect(rectangle)
-                context.drawPath(using: .fill)
-                context.fill(rectangle)
+                rectangle = CGRect(x: 10+10*index, y: (superviewHeight - 20) / 2, width: 2, height: 20)
             }
             else{
-                let rectangle = CGRect(x: 10+10*index, y: (superviewHeight - 12) / 2, width: 1, height: 12)
-                context.setFillColor(UIColor.white.cgColor)
-                context.addRect(rectangle)
-                context.drawPath(using: .fill)
-                context.fill(rectangle)
+                rectangle = CGRect(x: 10+10*index, y: (superviewHeight - 12) / 2, width: 1, height: 12)
             }
+            context.setFillColor(UIColor.white.cgColor)
+            context.addRect(rectangle)
+            context.drawPath(using: .fill)
+            context.fill(rectangle)
         }
     }
 }
